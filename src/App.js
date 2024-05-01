0a1e78896c2545a5b0122c4306c610011cb6c8d1
JHoww4 <jhonatanherique667@gmail.com>
2024-05-01 17:27:40 -0300

    relofio pre finalizado

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Relogio from "./Relogio";
+import Letreiro from "./Letreiro";
 
 function App() {
   return (
     <>
-      <h1>Fatec</h1>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <Letreiro/>    
+    <Relogio/>
     </>
   );
 }
-
 export default App;
--- src/Relogio.js
+++ src/Relogio.js
@@ -1,9 +1,32 @@
-function Relogio() {
-  return (
-    <>
-      <h1>Meu Relogio</h1>
-      
-    </>
-  );
+import React, { Component } from 'react';
+
+class Relogio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date()
+    };
+    this.intervalID = null;
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => {
+      this.setState({ time: new Date() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  render() {
+    const { time } = this.state;
+    return (
+      <div>
+        <h2>Relógio Digital</h2>
+        <p>Agora é {time.toLocaleTimeString()}.</p>
+      </div>
+    );
+  }
 }
 export default Relogio;
