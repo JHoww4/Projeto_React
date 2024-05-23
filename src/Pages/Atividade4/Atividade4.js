358f20f19ac0266e4b2621e40df8aeeff42de1ea
JHonatan_H <147053641+JHoww4@users.noreply.github.com>
2024-05-23 12:20:26 -0300

    Add files via upload

--- src/Pages/Atividade4/Atividade4.js
+++ src/Pages/Atividade4/Atividade4.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
+import App2 from './funcoes/Button';
+import Toolbar from './funcoes/ButtonTipo2';
 export default function Atividade4(){
     return(
         <>
             <h1>Atividade 4</h1>
             <br />
+            <App2 />
+            <hr />
+            <Toolbar/>
             <Link to="/">Retornar para página inicial</Link>
         </>
     );
