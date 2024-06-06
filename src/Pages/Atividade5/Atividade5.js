e54f00e29610ba2b3266dcd80d563fd5f743f123
JHoww4 <jhonatanherique667@gmail.com>
2024-06-05 22:06:41 -0300

    Atividade 5

---
+++ src/Pages/Atividade5/Atividade5.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+
+
+export default function Atividade5(){
+   
+    return(
+        <div>
+            <h1>Atividade 5</h1>
+            <br />
+            <Link to="/">Retornar para página inicial</Link>
+        </div>
+    );
+}
